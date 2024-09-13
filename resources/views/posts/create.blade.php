5f9f28f224adb80d99e5a9ded487c56c3d0dd102
Your Name <you@example.com>
2024-09-13 13:03:12 +0300

    post can be created

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- An unexamined life is not worth living. - Socrates -->
-</div>
+<x-app-layout>
+    <h1>Create post</h1>
+
+    <form action="{{ route('posts.store') }}" method="post">
+        @csrf
+        <label for="title">Title:</label>
+        <input type="text" id='title' name='title'>
+        <br>
+
+        <label for="content">Content:</label>
+        <textarea type="text" id='content' name='content'></textarea>
+        <br>
+
+        <input type="submit" value='Create'>
+    </form>
+</x-app-layout>
